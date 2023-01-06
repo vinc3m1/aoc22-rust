ef19e580cd6b25c6b52c5ae04ed3ef8769e7b01d
Vince Mi <vinc3m1@gmail.com>
2023-01-06 00:00:19 -0800

    cleaner logic without sort, and single while loop

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,5 +1,3 @@
-use std::include_str;
-
 pub fn day1() {
     println!("day1!");
 
@@ -7,38 +5,40 @@ pub fn day1() {
 
     let mut lines = contents.lines();
 
-    let mut line = lines.next();
-
     let mut max_calories = 0;
 
-    let mut all_calories = Vec::new();
+    let mut top3_calories = [0; 3];
 
-    // loop until end
-    while !(line.is_none()) {
-        let mut calories = 0;
+    let mut calories = 0;
 
+    // loop through all lines
+    while let Some(line) = lines.next() {
         // while lines are not empty, accumulate to current calorie count
-        while !(line.unwrap_or_default().is_empty()) {
-            calories = calories + line.unwrap().parse::<i32>().unwrap();
-            line = lines.next();
+        if !line.is_empty() {
+            calories += line.parse::<i32>().unwrap();
+
+            // loop and continue to sum calories
+            continue;
         }
 
-        // keep track of max calories
+        // otherwise, if the line is empty, track summed calories and reset count
+
+        // remember max calories
         if calories > max_calories {
             max_calories = calories;
         }
 
-        all_calories.push(calories);
+        // if any of the top 3 calories are smaller than the current elf's,
+        // find the smallest one and replace it with the current elf's calories
+        if let Some(min_top3) = top3_calories.iter_mut().filter(|x| **x < calories).min() {
+            *min_top3 = calories
+        }
 
-        // reset current calorie count after hitting empty line
-        line = lines.next();
+        // reset calories for next elf
+        calories = 0;
     }
 
     println!("max calories: {max_calories}");
-
-    all_calories.sort_unstable();
-    all_calories.reverse();
-    let top3_calories = &all_calories[..3];
 
     println!("top calories: {:?}", top3_calories);
 
