e20993afa904e2fe920731c45deb71d6230f2262
Vince Mi <vinc3m1@gmail.com>
2023-01-05 21:55:47 -0800

    add comments for day 1

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -11,18 +11,22 @@ pub fn day1() {
 
     let mut max_calories = 0;
 
+    // loop until end
     while !(line.is_none()) {
         let mut calories = 0;
 
+        // while lines are not empty, accumulate to current calorie count
         while !(line.unwrap_or_default().is_empty()) {
             calories = calories + line.unwrap().parse::<i32>().unwrap();
             line = lines.next();
         }
 
+        // keep track of max calories
         if calories > max_calories {
             max_calories = calories
         }
 
+        // reset current calorie count after hitting empty line
         line = lines.next();
     }
 
