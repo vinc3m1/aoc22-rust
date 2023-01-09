f0a154e05b891769247003e8341f67ccb13f9b14
Vince Mi <vinc3m1@gmail.com>
2023-01-08 19:18:52 -0800

    day5 init

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ mod days;
 fn main() {
     println!("Hello, world!");
 
-    let days = [days::day1::run, days::day2::run, days::day3::run, days::day4::run];
+    let days = [
+        days::day1::run,
+        days::day2::run,
+        days::day3::run,
+        days::day4::run,
+        days::day5::run,
+    ];
 
     for day in days {
         println!("---------------------------------");
