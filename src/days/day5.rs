54d214273be7de3b5cb038b72b467b99e4ed4614
Vince Mi <vinc3m1@gmail.com>
2023-01-08 22:12:28 -0800

    day5p2

--- src/days/day5.rs
+++ src/days/day5.rs
@@ -3,39 +3,48 @@ use itertools::Itertools;
 pub fn run() {
     println!("day5!");
 
-    let mut stacks = vec![
-        "DLJRVGF",  // 1
-        "TPMBVHJS", // 2
-        "VHMFDGPC", // 3
-        "MDPNGQ",   // 4
-        "JLHNF",    // 5
-        "NFVQDGTZ", // 6
-        "FDBL",     // 7
-        "MJBSVDN",  // 8
-        "GLD",      // 9
-    ]
-    .iter()
-    .map(|s| s.chars().collect_vec())
-    .collect_vec();
+    let mut stacks1 = vec![
+        String::from("DLJRVGF"),  // 1
+        String::from("TPMBVHJS"), // 2
+        String::from("VHMFDGPC"), // 3
+        String::from("MDPNGQ"),   // 4
+        String::from("JLHNF"),    // 5
+        String::from("NFVQDGTZ"), // 6
+        String::from("FDBL"),     // 7
+        String::from("MJBSVDN"),  // 8
+        String::from("GLD"),      // 9
+    ];
+    let mut stacks2 = stacks1.clone();
 
     for line in include_str!("day5.txt").lines() {
+        // Pattern match count, from, to as strings
         if let Some(("move", count_s, "from", from_s, "to", to_s)) = line.split(" ").collect_tuple()
         {
-            let count = count_s.parse::<i32>().unwrap();
-
-            // subtract 1 to change from 1 index to 0 index
+            // parse strings into usize
+            let count = count_s.parse::<usize>().unwrap();
+            // subtract 1 from from/to to change from 1 index to 0 index
             let from = from_s.parse::<usize>().unwrap() - 1;
             let to = to_s.parse::<usize>().unwrap() - 1;
 
+            // part 1, move each crate one by one
             for _ in 0..count {
                 // stack_to.push(stack_from.pop().unwrap());
-                let item = stacks[from].pop().unwrap();
-                stacks[to].push(item);
+                let item = stacks1[from].pop().unwrap();
+                stacks1[to].push(item);
             }
+
+            // part 2, move all at once
+            let split_idx = stacks2[from].len() - count;
+            let moving = stacks2[from].split_off(split_idx);
+            stacks2[to].push_str(&moving);
         }
     }
 
-    let top_stacks: String = stacks.iter().map(|s| s.last().unwrap()).collect();
+    let top_stacks1: String = stacks1.iter().map(|s| s.chars().last().unwrap()).collect();
+    println!("top of stacks pt 1: {top_stacks1}");
+    assert_eq!(top_stacks1, "QMBMJDFTD");
 
-    println!("top of stacks: {top_stacks}");
+    let top_stacks2: String = stacks2.iter().map(|s| s.chars().last().unwrap()).collect();
+    println!("top of stacks pt 2: {top_stacks2}");
+    assert_eq!(top_stacks2, "NBTVTJNFJ");
 }
