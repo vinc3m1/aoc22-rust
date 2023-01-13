1caee87164a75f438a92a1c8dfd330b71a05c939
Vince Mi <vinc3m1@gmail.com>
2023-01-13 12:27:45 -0800

    day9 init

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod day5;
 mod day6;
 mod day7;
 mod day8;
+mod day9;
 
 fn main() {
     println!("Hello, world!");
@@ -19,6 +20,7 @@ fn main() {
         day6::run,
         day7::run,
         day8::run,
+        day9::run,
     ];
 
     for day in days {
